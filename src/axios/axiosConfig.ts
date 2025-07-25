41b572db75d9caf495cab08d835168de2b02bc52
nazaninJh <nazanin.jh97@gmail.com>
2025-07-25 22:31:41 +0330

    Update axios config

--- src/axios/axiosConfig.ts
+++ src/axios/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosCustom = axios.create({
-  baseURL: "/api",
+  baseURL: `${process.env.DOMAIN}/api` || "/api",
   withCredentials: true,
   timeout: 5000,
   timeoutErrorMessage: "No response was sent from the server, timeout!",
